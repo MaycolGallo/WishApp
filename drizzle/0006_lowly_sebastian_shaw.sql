6afcf13314b8e043c2e70a05729e8d370f840511
MaycolGallo <maycolgallo25@gmail.com>
2025-08-05 10:00:41 -0500

    clean development build

--- drizzle/0006_lowly_sebastian_shaw.sql
+++ drizzle/0006_lowly_sebastian_shaw.sql
@@ -1,14 +1,22 @@
-DROP TABLE `products`;--> statement-breakpoint
-PRAGMA foreign_keys=OFF;--> statement-breakpoint
+DROP TABLE `products`;
+--> statement-breakpoint
+PRAGMA foreign_keys = OFF;
+--> statement-breakpoint
 CREATE TABLE `__new_wishlist_categories` (
-	`wishlistId` integer NOT NULL,
-	`categoryId` integer NOT NULL,
-	FOREIGN KEY (`wishlistId`) REFERENCES `wishlists`(`id`) ON UPDATE no action ON DELETE no action,
-	FOREIGN KEY (`categoryId`) REFERENCES `categories`(`id`) ON UPDATE no action ON DELETE no action
+    `wishlistId` integer NOT NULL,
+    `categoryId` integer NOT NULL,
+    FOREIGN KEY (`wishlistId`) REFERENCES `wishlists` (`id`) ON UPDATE no action ON DELETE no action,
+    FOREIGN KEY (`categoryId`) REFERENCES `categories` (`id`) ON UPDATE no action ON DELETE no action
 );
 --> statement-breakpoint
-INSERT INTO `__new_wishlist_categories`("wishlistId", "categoryId") SELECT "wishlistId", "categoryId" FROM `wishlist_categories`;--> statement-breakpoint
-DROP TABLE `wishlist_categories`;--> statement-breakpoint
-ALTER TABLE `__new_wishlist_categories` RENAME TO `wishlist_categories`;--> statement-breakpoint
-DELETE FROM wishlist WHERE name like '%64%';
-PRAGMA foreign_keys=ON;
+INSERT INTO
+    `__new_wishlist_categories` ("wishlistId", "categoryId")
+SELECT "wishlistId", "categoryId"
+FROM `wishlist_categories`;
+--> statement-breakpoint
+DROP TABLE `wishlist_categories`;
+--> statement-breakpoint
+ALTER TABLE `__new_wishlist_categories`
+RENAME TO `wishlist_categories`;
+--> statement-breakpoint
+PRAGMA foreign_keys = ON;
