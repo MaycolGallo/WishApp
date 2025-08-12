81e477a0d1fb836d4e1ee5708a2541ffbc3a02fa
elpep <elpep@gmail.com>
2025-08-11 19:22:01 -0500

    fun

--- lib/db.ts
+++ lib/db.ts
@@ -1,6 +1,6 @@
-import { drizzle } from 'drizzle-orm/expo-sqlite';
-import { openDatabaseSync } from 'expo-sqlite';
-import * as schema from '../db/schema';
+import { drizzle } from "drizzle-orm/expo-sqlite";
+import { openDatabaseSync } from "expo-sqlite";
+import * as schema from "../db/schema";
 
-const expoDb = openDatabaseSync('wishlists');
+const expoDb = openDatabaseSync("wishlists", { enableChangeListener: true });
 export const db = drizzle(expoDb, { schema, logger: true });
